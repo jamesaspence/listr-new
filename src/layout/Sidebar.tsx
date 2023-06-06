84fdc4daead9f7ae95cc2a3c88dd8721e769653d
James Spence <jamesaspence20@gmail.com>
2023-06-05 23:18:05 -0500

    style: sidebar layout style tweaks

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,7 +1,8 @@
 import { ReactNode } from 'react';
+import styles from './Sidebar.module.css';
 
 export type SidebarProps = {
   children: ReactNode;
 };
 
-export const Sidebar = ({ children }: SidebarProps) => <div>{children}</div>;
+export const Sidebar = ({ children }: SidebarProps) => <div className={styles.sidebar}>{children}</div>;
