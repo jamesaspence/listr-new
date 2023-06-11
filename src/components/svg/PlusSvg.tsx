c3c794bc4559270e26eb055afbb653f3f0ad7e8d
James Spence <Jamesaspence20@gmail.com>
2023-06-11 16:14:51 -0700

    style: remove circle from edit pencil svg, add common svg prop type

--- src/components/svg/PlusSvg.tsx
+++ src/components/svg/PlusSvg.tsx
@@ -1,8 +1,8 @@
 import styles from './PlusSvg.module.css';
-import { SVGAttributes } from 'react';
 import * as classNames from 'classnames';
+import { CommonSvgProps } from './util.ts';
 
-export type PlusSvgProps = Omit<SVGAttributes<SVGElement>, 'viewBox'>;
+export type PlusSvgProps = CommonSvgProps;
 
 export const PlusSvg = ({ className, ...props }: PlusSvgProps) => (
   <svg
@@ -10,8 +10,7 @@ export const PlusSvg = ({ className, ...props }: PlusSvgProps) => (
     viewBox="0 0 16 16"
     {...props}
   >
-    <circle className={styles.circle} cx="8" cy="8" r="7" />
-    <polyline className={styles.plusLine} points="4,8 12,8" />
-    <polyline className={styles.plusLine} points="8,4 8,12" />
+    <polyline className={styles.plusLine} points="3,8 13,8" />
+    <polyline className={styles.plusLine} points="8,3 8,13" />
   </svg>
 );
---
+++ src/components/svg/util.ts
@@ -0,0 +1,3 @@
+import { SVGAttributes } from 'react';
+
+export type CommonSvgProps = Omit<SVGAttributes<SVGElement>, 'viewBox'>;
