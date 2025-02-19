a06cba909d6b3765580baceadf74ae982fe5b190
James Spence <jamesaspence20@gmail.com>
2025-02-19 00:02:30 -0600

    feat: working theme change button in header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,23 @@
 import packageInfo from '../../../package.json';
 import styles from './Header.module.css';
 import logo from '../../logo.svg';
+import { ChangeThemeSvg } from '../svg/ChangeThemeSvg.tsx';
+import { useTheme } from '../../hooks/useTheme.ts';
 
-export const Header = () => (
-  <header className={styles.header}>
-    <img className={styles.logo} src={logo} alt="Listr logo" />
-    <div className={styles.textWrapper}>
-      <h1 className={styles.headerText}>Listr</h1>
-      <small className={styles.headerVersion}>v{packageInfo.version}</small>
-    </div>
-  </header>
-);
+export const Header = () => {
+  const [theme, setTheme] = useTheme();
+
+  return (
+    <header className={styles.header}>
+      <img className={styles.logo} src={logo} alt="Listr logo" />
+      <div className={styles.textWrapper}>
+        <h1 className={styles.headerText}>Listr</h1>
+        <small className={styles.headerVersion}>v{packageInfo.version}</small>
+      </div>
+      <ChangeThemeSvg
+        className={styles.themeButton}
+        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      />
+    </header>
+  );
+};
