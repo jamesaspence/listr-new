d5a3ee7ef35fb4b4ddfcde0c9e35481e63f97607
James Spence <jamesaspence20@gmail.com>
2025-03-04 21:54:22 -0600

    feat: disable themes for now

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,12 +1,8 @@
 import packageInfo from '../../../package.json';
 import styles from './Header.module.css';
 import logo from '../../logo.svg';
-import { ChangeThemeSvg } from '../svg/ChangeThemeSvg.tsx';
-import { useTheme } from '../../hooks/useTheme.ts';
 
 export const Header = () => {
-  const [theme, setTheme] = useTheme();
-
   return (
     <header className={styles.header}>
       <img className={styles.logo} src={logo} alt="Listr logo" />
@@ -14,10 +10,6 @@ export const Header = () => {
         <h1 className={styles.headerText}>Listr</h1>
         <small className={styles.headerVersion}>v{packageInfo.version}</small>
       </div>
-      <ChangeThemeSvg
-        className={styles.themeButton}
-        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-      />
     </header>
   );
 };
