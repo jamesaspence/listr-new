fde79f299b570e32982053766b93f5a5faa6d1c4
James Spence <jamesaspence20@gmail.com>
2025-03-06 00:58:28 -0600

    feat: wip collapsible sidebar with missing hover state

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -6,15 +6,21 @@ import { useSelector } from 'react-redux';
 import { selectActiveListId, selectLists } from '../../redux/slices/list.ts';
 import { SidebarHandle } from './SidebarHandle.tsx';
 import packageInfo from '../../../package.json';
+import { useState } from 'react';
 
 export const Sidebar = () => {
   const activeListId = useSelector(selectActiveListId);
   const lists = useSelector(selectLists);
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className={classNames(styles.sidebar, styles.open)}>
-      <SidebarHandle />
-      <div className={styles.sidebarContents}>
+    <div
+      className={classNames(styles.sidebar, {
+        [styles.open]: open,
+      })}
+    >
+      <SidebarHandle onToggleOpen={() => setOpen(!open)} />
+      <div className={classNames(styles.sidebarContents)}>
         <div className={styles.sidebarContentHeader}>
           <h5 className={styles.headerText}>Listr</h5>
           <small className={styles.headerVersion}>v{packageInfo.version}</small>
--- src/components/sidebar/SidebarHandle.tsx
+++ src/components/sidebar/SidebarHandle.tsx
@@ -1,10 +1,14 @@
 import styles from './SidebarHandle.module.css';
 import { LogoSvg } from '../svg/LogoSvg.tsx';
 
-export const SidebarHandle = () => {
+export type SidebarHandleProps = {
+  onToggleOpen: () => void;
+};
+
+export const SidebarHandle = ({ onToggleOpen }: SidebarHandleProps) => {
   return (
     <div className={styles.sidebarHandle}>
-      <LogoSvg className={styles.sidebarLogo} />
+      <LogoSvg className={styles.sidebarLogo} onClick={onToggleOpen} />
     </div>
   );
 };
